0387b0606f5f93e3f98b20d5333e6865271ade6b
franciscoalfaro <franciscoalfar@gmail.com>
2026-05-25 14:26:59 -0400

    mejora

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ import recoveryRoutes from "./modules/recovery/recovery.routes.js";
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(
     helmet({
         crossOriginResourcePolicy: {
